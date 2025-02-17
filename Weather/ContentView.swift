89a5d51a3e7eeec12fc173809824acd3464b3fd7
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-02-17 14:38:12 +0330

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Sothesom on 29/11/1403.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
